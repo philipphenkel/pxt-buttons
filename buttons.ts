2ceda4e01aec687a8849bb0b6d7522fff54982b6
Philipp Henkel <philipphenkel@users.noreply.github.com>
2021-07-29 07:33:48 +0200

    Add code to shim

--- buttons.ts
+++ buttons.ts
@@ -4,12 +4,12 @@ namespace buttons {
     * Enable the micro:bit buttons. 
     */
     //% shim=buttons::enable
-    export function enable(): void { }
+    export function enable(): void { basic.pause(100) }
 
     /**
     * Disable the micro:bit buttons. 
     */
     //% shim=buttons::disable
-    export function disable(): void { }
+    export function disable(): void { basic.pause(100) }
 
 }
